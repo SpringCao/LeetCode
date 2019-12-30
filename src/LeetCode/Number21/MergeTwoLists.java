56d6e78d0c2ef8e13f32864e55b6ed4a24e270cd
SpringCao <1580730107@qq.com>
2019-12-30 13:21:16 +0800

    添加量一个.gitignore文件

--- src/LeetCode/Number21/MergeTwoLists.java
+++ src/LeetCode/Number21/MergeTwoLists.java
@@ -18,13 +18,13 @@ public class MergeTwoLists {
         int n=10;
         for(int i=0;i<n;i++){
             ListNode p=new ListNode(i);
-            ListNode q=new ListNode(i);
+            ListNode q=new ListNode(i+1);
             l1.next=p;
             l2.next=q;
             l1=p;
             l2=q;
         }
-        ListNode res=mergeTwoLists(lpre1.next,lpre2.next);
+        ListNode res=mergeTwoLists2(lpre1.next,lpre2.next);
         while (res!=null){
             System.out.println(res.value);
             res=res.next;
@@ -49,5 +49,21 @@ public class MergeTwoLists {
         if (l1 == null) pre1.next = l2;
         if (l2 == null) pre1.next = l1;
         return pre.next;
+    }
+
+    //法二：递归
+    private static ListNode mergeTwoLists2(ListNode l1,ListNode l2){
+        ListNode pre = new ListNode(-1);
+        ListNode pre1 = pre;
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+        if (l1.value <= l2.value){
+            l1.next = mergeTwoLists2(l1.next,l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists2(l2.next,l1);
+            return l2;
+        }
     }
 }
